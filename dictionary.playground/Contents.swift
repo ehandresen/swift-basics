7ed487a23fe73905bef96b98d39741b09825c92d
Eirik Andresen <eirik.horgen.andresen@gmail.com>
2024-08-25 14:13:09 +0200

    dictionary

---
+++ dictionary.playground/Contents.swift
@@ -0,0 +1,12 @@
+//          Dictionary
+
+// stores data in key/value pairs
+// constant time lookup (very fast), similar to Set
+
+let devices: [String: String] = [
+    "phone": "iphone se",
+    "laptop": "macbook m1",
+    "desktop": "huawei matebook pro"
+]
+
+let phone = devices["phone"] // accessing the value of the key
---
+++ test.swift
@@ -0,0 +1,10 @@
+//
+//  test.swift
+//  
+//
+//  Created by Eirik Andresen on 25/08/2024.
+//
+
+import Foundation
+
+let name = "eirik";
