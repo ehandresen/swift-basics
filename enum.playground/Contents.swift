a6174e8c8cc7a802306bf36fdb3a8ff8b9595cf2
Eirik Andresen <eirik.horgen.andresen@gmail.com>
2024-08-25 21:29:17 +0200

    enum

--- enum.playground/Contents.swift
+++ enum.playground/Contents.swift
@@ -1,3 +1,12 @@
-import UIKit
+enum Phone: String {
+    case iphone11pro    = "pro"
+    case iphonese       = "apple hardon"
+    case samsungS22     = "samsung gucci"
+    case pixel4         = "google it"
+}
 
-var greeting = "Hello, playground"
+func getMyOpinion(on phone: Phone) {
+    print(phone.rawValue)
+}
+
+getMyOpinion(on: .pixel4)
